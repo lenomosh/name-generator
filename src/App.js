1ca40201c9d3e76988450752c36a6a3c07f82584
lenomosh <omondilenox@outlook.com>
2020-07-18 11:37:19 +0300

    completed dget day of week

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import './App.css';
+import './App.scss';
 export class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            dateIsInvalid: false,
+            genderIsInvalid: false,
+            date: '',
+            gender: '',
 
         }
     }
@@ -37,28 +41,60 @@ export class App extends Component {
             female: 'Ama'
         },
     }
-    dayOfTheWeek = (centuryDigits, yearDigits, month, day) => ((((centuryDigits / 4) - 2 * centuryDigits - 1) + ((5 * yearDigits / 4)) + ((26 * (month + 1) / 10)) + day) % 7)
+
     dateValidation = (date) => {
         const dateFormat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
-        !date.match(dateFormat) && ("invalid date format")
+        if (date.match(dateFormat)) {
+            this.setState({ dateIsInvalid: true })
+        }
 
     }
+    genderValidation = (gender) => ((gender !== 'm' || gender !== 'f') && this.setState({ genderIsInvalid: true }))
+    splitDate = date =>{
+        const dateArray =date.split('-').join('/').split('/')
+        return {
+            year:dateArray[0],
+            month:dateArray[1],
+            day:dateArray[2],
+        }
+    }
+    handleForm = (event) => {
+        event.preventDefault()
+        const form = new FormData(event.target)
+        const date = form.get('date')
+        const gender = form.get('gender')
+        /** */
+        const splittedDate =this.splitDate(date)
+        const centuryDigits = parseInt(splittedDate.year.toString().slice(0,2));
+        // console.log(centuryDigits);
+        const yearDigits =parseInt(splittedDate.year.toString().slice(2));
+        console.log(yearDigits);
+        // const dayBorn = this.getDayOfTheWeek(centuryDigits,yearDigits,splittedDate.month,splittedDate.day)
+        const newDate = new Date(date)
+        const dayBorn = newDate.getDay()
+        console.log(dayBorn)
+
+
+    }
+    handleDateChange = date => this.setState({ date })
+    handleGenderChange = gender => this.setState({ gender })
     render() {
         return (
             <div className="main-wrapper">
                 <div className="form-wrapper">
-                    <form onChange={(event) => console.log(event)}>
+                    <form onSubmit={(event) => this.handleForm(event)} onChange={(event) => console.log(event)}>
                         <div className="input-wrapper">
-                            <input type='date' />
-                            <select>
+                            <input name="date" onChange={(event) => this.dateValidation(event.target.value)} type='date' />
+                            {this.state.dateIsInvalid && <p>The date is invalid</p>}
+                            <select name="gender">
                                 <option value='m'>Male</option>
                                 <option value='f'>Female</option>
                             </select>
-                            <input type='submit' value="Submit"/>
+                            {this.state.genderIsInvalid && <p>The gender is invalid</p>}
+                            <input type='submit' value="Submit" />
                         </div>
                     </form>
                 </div>
-
             </div>
         )
     }
