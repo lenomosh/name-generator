98df8a1149af3dbed95df9910129a6054c452765
lenomosh <omondilenox@outlook.com>
2020-07-19 14:16:10 +0300

    changed app component from class to function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.scss';
-export class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            dateIsInvalid: false,
-            genderIsInvalid: false,
-            akanName: null,
-            proceed: false,
-            currentStep: 1
-        }
-    }
-    names = {
+const App =()=>{
+        const [genderIsInvalid, setGenderIsInvalid] = useState(false)
+        const [dateIsInvalid, setDateIsInvalid] = useState(false);
+        const [akanName, setAkanName] = useState(null)
+        const [currentStep, setCurrentStep] = useState(1)
+
+   const  names = {
         Sunday: {
             male: 'Kwasi',
             female: 'Akosua'
@@ -41,53 +36,58 @@ export class App extends Component {
             female: 'Ama'
         },
     }
-
-    dateValidation = date => {
+    const dateValidation = date => {
         console.log('datefrom dateValidation', date);
         const dateFormat = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
         console.log('match', dateFormat.test(date));
         if (!dateFormat.test(date) || date === null || date === '' || date === undefined) {
-            this.setState({ dateIsInvalid: true })
+            // setState({ dateIsInvalid: true })
+            setDateIsInvalid(true)
             return false
         } else {
-            this.setState({ dateIsInvalid: false })
+            // setState({ dateIsInvalid: false })
+            setDateIsInvalid(false)
             return true
         }
 
     }
-    genderValidation = gender => ((!gender.match(/male|female/gi) || gender === null || gender === undefined) && this.setState({ genderIsInvalid: true }))
-    handleForm = event => {
+    const genderValidation = gender => ((!gender.match(/male|female/gi) || gender === null || gender === undefined) && setGenderIsInvalid( true ))
+    const handleForm = event => {
         event.preventDefault()
-        const { dateIsInvalid, genderIsInvalid } = this.state;
+        // const { dateIsInvalid, genderIsInvalid } = state;
         const form = new FormData(event.target)
         const date = form.get('date')
         const gender = form.get('gender')
-        this.dateValidation(date)
-        this.genderValidation(gender)
+        dateValidation(date)
+        genderValidation(gender)
         if (dateIsInvalid === false && date !== undefined && date.length > 9 && gender.length >= 4 && genderIsInvalid === false) {
             const newDate = new Date(date)
             const dayBorn = newDate.getDay()
-            const daysOfTheWeek = Object.keys(this.names)
+            const daysOfTheWeek = Object.keys(names)
             console.log(daysOfTheWeek);
             console.log(dayBorn)
-            const akanName = this.names[daysOfTheWeek[dayBorn]][gender]
-            this.setState({ akanName })
-            this.handleStepIncrement('next')
+            const akanName = names[daysOfTheWeek[dayBorn]][gender]
+            setAkanName( akanName)
+            handleStepIncrement('next')
         }
     }
-    handleDateChange = date => {
-        this.dateValidation(date);
+    const handleDateChange = date => {
+        dateValidation(date);
     }
-    handleGenderChange = gender => {
-        this.genderValidation(gender)
+    const handleGenderChange = gender => {
+        genderValidation(gender)
     }
-    handleStepIncrement = (type = 'next') => {
-        type === 'next' ? this.setState(prevState => ({ currentStep: prevState.currentStep++ }))
-            :
-            this.setState(prevState => ({ currentStep: prevState.currentStep-- }))
+    const handleStepIncrement = (stepType='next') => {
+        console.log(stepType);
+        if (stepType === 'next') {
+            setCurrentStep(currentStep+1)
+
+        } else {
+            setCurrentStep(currentStep- 1)
+
+        }
+
     }
-    render() {
-        const { dateIsInvalid, genderIsInvalid, akanName, currentStep } = this.state;
 
         return (
             <div className="main-wrapper">
@@ -99,22 +99,22 @@ export class App extends Component {
 
                             This is a web application that takes a user's birthday and calculates the day of the week they were born and then depending on their gender outputs their Akan Name. Akan names are derived from Ghanian culture. Frequently in Ghana, children are given their first name as a 'day name' which corresponds to the day in the week they were born.
                         </p>
-                        <button onClick={() => this.handleStepIncrement('next')} className="button">Proceed to next step</button>
+                        <button onClick={()=>handleStepIncrement()} className="button">Proceed to next step</button>
 
 
                     </div>}
                 {currentStep === 2 &&
 
                     <div className="form-wrapper">
-                        <form onSubmit={event => this.handleForm(event)}>
+                        <form onSubmit={event => handleForm(event)}>
                             <div className="input-wrapper">
                                 <label for="date">Date of Birth</label>
-                                <input id="date" className="input-item" name="date" onChange={event => this.handleDateChange(event.target.value)} type='date' />
+                                <input id="date" className="input-item" name="date" onChange={event => handleDateChange(event.target.value)} type='date' />
                                 {dateIsInvalid && <p className="input-error">The date is either blank or invalid</p>}
                             </div>
                             <div className="input-wrapper">
                                 <label for="gender">Date of Birth</label>
-                                <select id='gender' onChange={event => this.handleGenderChange(event.target.value)} className="input-item" name="gender">
+                                <select id='gender' onChange={event => handleGenderChange(event.target.value)} className="input-item" name="gender">
                                     <option value='male'>Male</option>
                                     <option value='female'>Female</option>
                                 </select>
@@ -124,7 +124,7 @@ export class App extends Component {
                                 {(dateIsInvalid === false && genderIsInvalid === false) && <input className="submit" type='submit' value="Get Akan Name" />}
 
                             </div>
-                            <button onClick={() => this.handleStepIncrement('prev')} className="button">Previous Step</button>
+                            <button onClick={() => handleStepIncrement('prev')} className="button">Previous Step</button>
 
                         </form>
 
@@ -135,12 +135,11 @@ export class App extends Component {
 
                     <div className="akanName-wrapper">
                         <p>Congratulations, your Akan Name is {akanName}</p>
-                        <button onClick={() => this.handleStepIncrement('prev')} className="button">Previous Step</button>
+                        <button onClick={() => handleStepIncrement('prev')} className="button">Previous Step</button>
                     </div>
 
                 }
             </div>
         )
     }
-}
 export default App
