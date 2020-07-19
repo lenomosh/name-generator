118e924016609a3659d82446ea086027374aeddd
lenomosh <omondilenox@outlook.com>
2020-07-19 11:18:03 +0300

    Updates

---
+++ precache-manifest.32b200169381cd991caa4f42784eb562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb1c59fb79135dd73ace99449d74ad1",
+    "url": "/name-generator/index.html"
+  },
+  {
+    "revision": "a204cf2ba6014900f3ee",
+    "url": "/name-generator/static/css/main.2c2687c0.chunk.css"
+  },
+  {
+    "revision": "f8cf88f86c6e5ebbc365",
+    "url": "/name-generator/static/js/2.0901e8a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/name-generator/static/js/2.0901e8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a204cf2ba6014900f3ee",
+    "url": "/name-generator/static/js/main.e0f91671.chunk.js"
+  },
+  {
+    "revision": "9fe3ed42e0fa3fdcbde0",
+    "url": "/name-generator/static/js/runtime-main.bdbe69e9.js"
+  }
+]);
